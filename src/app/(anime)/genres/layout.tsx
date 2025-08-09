208814791b41533ec3944988df1a98f871f20fa9
Visualcodepo <156519759+Dikrey@users.noreply.github.com>
2025-08-09 22:08:34 +0700

    Update layout.tsx

--- src/app/(anime)/genres/layout.tsx
+++ src/app/(anime)/genres/layout.tsx
@@ -3,7 +3,7 @@ import { Metadata } from "next/types";
 
 export const metadata: Metadata = {
   title: "Genres | Otakudesu",
-  description: "Genres Page Otakudesu. Build by Rizky Haksono",
+  description: "Genres Page Otakudesu. Build by Raihan_official0307",
 };
 
 export default function GenreLayout({
