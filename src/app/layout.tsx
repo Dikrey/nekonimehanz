50f9fe2c19e8b2b741da353a9747d647692ca680
Visualcodepo <156519759+Dikrey@users.noreply.github.com>
2025-08-10 12:59:21 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import ProviderLayout from "./provider";
@@ -10,7 +11,7 @@ const fontInter = Inter({
 });
 
 export const metadata: Metadata = {
-  title: "Otakudesu",
+  title: "Animenekohanz",
   description: "Build by Raihan_official0307",
 };
 
@@ -19,11 +20,20 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
+      <head>
+        {/* Script Iklan AdSense Global */}
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1048374829701737"
+          crossOrigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </head>
       <body
         suppressHydrationWarning={true}
         className={cn(
           "min-h-screen bg-background font-sans antialiased",
-          fontInter.className,
+          fontInter.className
         )}
       >
         <ProviderLayout>{children}</ProviderLayout>
